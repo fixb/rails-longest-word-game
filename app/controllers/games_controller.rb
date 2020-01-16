0003df841eb97bd1d63d56fc6423caa09a643695
Francois Baud <fxbaud@gmail.com>
2020-01-16 21:07:15 +0000

    with css

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,27 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
-    @letters = Array.new(8) { ('A'..'Z').to_a.sample }
+    @letters = Array.new(10) { ('a'..'z').to_a.sample }
   end
 
   def score
-    raise
+    @grid = params[:grid]
+    @word = params[:word]
+
+    if @word.chars.all? { |letter| @word.count(letter) <= @grid.count(letter) }
+      response = open("https://wagon-dictionary.herokuapp.com/#{@word}")
+        json = JSON.parse(response.read)
+        if json["found"] == true
+          @result = 1
+          # @result = "Congratulation, #{@word.upcase} is a valid English word!"
+        else
+          @result = 2
+    end
+    else
+      @result = 3
+      # "Sorry but #{@word.upcase} can't be built out of #{@grid}..."
+    end
   end
 end
